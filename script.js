d0e028ef4ebd6472f5f6350097e2aa3a252782ca
celestinkaske <132942345+celestinkaske@users.noreply.github.com>
2023-05-17 20:59:37 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const terms = [
+    { term: "API"}
+    { term: "Backend"}
+    { term: "Cross-Docking"}
+    { term: "Data-Driven-Marketing"}
+    { term: "Data-Warehouse"}
+    { term: "HTTPS"}
+]
+
+
+function getVal() {
+    const val = document.querySelector('input').value;
+    console.log(val);
+  }
