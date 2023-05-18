344c55964b7bd8a6bd8b28d6a425950941cecf32
celestinkaske <132942345+celestinkaske@users.noreply.github.com>
2023-05-18 18:15:36 +0200

    feat: add dynamic search

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
 const terms = [
-    { term: "API"}
-    { term: "Backend"}
-    { term: "Cross-Docking"}
-    { term: "Data-Driven-Marketing"}
-    { term: "Data-Warehouse"}
-    { term: "HTTPS"}
+    { term: "API" },
+    { term: "API1" },
+    { term: "API2" },
+    { term: "API3" },
+    { term: "Backend" },
+    { term: "Cross-Docking" },
+    { term: "Data-Driven-Marketing" },
+    { term: "Data-Warehouse" },
+    { term: "HTTPS" },
 ]
 
+const alphabet = ['A', 'B', 'C', 'D', 'H']
 
-function getVal() {
-    const val = document.querySelector('input').value;
-    console.log(val);
-  }
+
+
+const input = document.getElementById('search')
+input.addEventListener('input', function (event) {
+    const currentValue = event.target.value;
+    const firstCharacter = currentValue.charAt(0).toUpperCase()
+    const isEmpty = currentValue === ""
+    console.log(currentValue)
+
+
+    console.log(firstCharacter)
+
+    alphabet.forEach((letter) => {
+        const sectionElement = document.getElementById(letter)
+        if (letter === firstCharacter || isEmpty) {
+            sectionElement.classList.remove('hidden')
+        } else {
+            sectionElement.classList.add('hidden')
+        }
+    })
+
+    terms.forEach((item) => {
+        const itemExists = item.term.toLowerCase().startsWith(currentValue.toLowerCase())
+        const element = document.getElementById(item.term)
+        console.log(itemExists, element, isEmpty)
+        if (itemExists || isEmpty) {
+            element.classList.remove('hidden')
+        } else {
+            element.classList.add('hidden')
+        }
+    })
+
+})
+
+
+const date = new Date(document.lastModified);
+const formattedDate = new Intl.DateTimeFormat("de-DE", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+}).format(date)
+
+document.getElementById("last-modified-date").innerHTML = "Letzte Speicherung : " + formattedDate;
