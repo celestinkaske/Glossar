c029a69d9ce51de1ac26e8a94aa28e1bde8b39de
celestinkaske <132942345+celestinkaske@users.noreply.github.com>
2023-05-22 22:04:11 +0200

    Chore: Added Date on Pages

--- script.js
+++ script.js
@@ -19,38 +19,40 @@ const alphabet = ['A', 'B', 'C', 'D']
 
 
 const input = document.getElementById('search')
-input.addEventListener('input', function (event) {
-    const currentValue = event.target.value;
-    const firstCharacter = currentValue.charAt(0).toUpperCase()
-    const isEmpty = currentValue === ""
-    console.log(currentValue)
+if (input) {
+    input.addEventListener('input', function (event) {
+        const currentValue = event.target.value;
+        const firstCharacter = currentValue.charAt(0).toUpperCase()
+        const isEmpty = currentValue === ""
+        console.log(currentValue)
 
 
-    console.log(firstCharacter)
+        console.log(firstCharacter)
 
-    // hide letter sections
-    alphabet.forEach((letter) => {
-        const sectionElement = document.getElementById(letter)
-        if (letter === firstCharacter || isEmpty) {
-            sectionElement.classList.remove('hidden')
-        } else {
-            sectionElement.classList.add('hidden')
-        }
+        // hide letter sections
+        alphabet.forEach((letter) => {
+            const sectionElement = document.getElementById(letter)
+            if (letter === firstCharacter || isEmpty) {
+                sectionElement.classList.remove('hidden')
+            } else {
+                sectionElement.classList.add('hidden')
+            }
+        })
+
+        // hide terms in sections
+        terms.forEach((item) => {
+            const itemExists = item.term.toLowerCase().startsWith(currentValue.toLowerCase())
+            const element = document.getElementById(item.term)
+            console.log(itemExists, element, isEmpty)
+            if (itemExists || isEmpty) {
+                element.classList.remove('hidden')
+            } else {
+                element.classList.add('hidden')
+            }
+        })
+
     })
-
-    // hide terms in sections
-    terms.forEach((item) => {
-        const itemExists = item.term.toLowerCase().startsWith(currentValue.toLowerCase())
-        const element = document.getElementById(item.term)
-        console.log(itemExists, element, isEmpty)
-        if (itemExists || isEmpty) {
-            element.classList.remove('hidden')
-        } else {
-            element.classList.add('hidden')
-        }
-    })
-
-})
+}
 
 // place formatted last modified date
 const date = new Date(document.lastModified);
